32dca43fceaaea7dfdb8dbd1a19b9d012ae11278
QuinnBerryman <quinn.berryman@ucdconnect.ie>
2020-09-27 20:56:29 +0100

    Finished feature 2

--- test.c
+++ test.c
@@ -3,24 +3,35 @@
 
 int confirm;
 int age;
+int begin = 1;
 int confirmer ();
 
 int main () {
-  printf("Hi, Would you like to begin?\t");
   confirmer();
-  printf("Ok, first question. How old are you?\t");
+  printf("\nHow old are you?\t");
   scanf("%d",&age);
-  printf("Cool you are %d years old.", age);
+  printf("\nCool you are %d years old.", age);
   confirmer();
 }
 
 int confirmer() {
-  printf("\nWould you like to continue?\t");
-  scanf("%d", &confirm);
-  if (!confirm) {
-    printf("Goodbye!");
-    exit(0);
+  if (begin == 0) {
+    printf("\nWould you like to continue?\t");
+    scanf("%d", &confirm);
+    if (!confirm) {
+      printf("Goodbye!");
+      exit(0);
+    }
+    printf("\nOk next question.");
+    return 0;
+  } else if (begin == 1) {
+    begin = 0;
+    printf("Hi, Would you like to begin?\t");
+    scanf("%d", &confirm);
+    if (!confirm) {
+      printf("Goodbye!");
+      exit(0);
+    }
+    printf("Ok, first question.");
   }
-  printf("\nOk next question.");
-  return 0;
 }
