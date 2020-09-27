e942ebbb83dcad27ef21f3dd91454c9fe44abd27
QuinnBerryman <quinn.berryman@ucdconnect.ie>
2020-09-27 20:47:08 +0100

    Begun work on feature two, added a confirmation function.

--- test.c
+++ test.c
@@ -3,16 +3,24 @@
 
 int confirm;
 int age;
+int confirmer ();
 
 int main () {
   printf("Hi, Would you like to begin?\t");
-  scanf("%d", &confirm);
-  if (!confirm) {
-    return 0;
-    printf("Goodbye!");
-  }
-  printf("Thank you for continuing!\n");
+  confirmer();
   printf("Ok, first question. How old are you?\t");
   scanf("%d",&age);
-  printf("Cool you are %d years old.\nWould you like to answer another question?\t", age);
+  printf("Cool you are %d years old.", age);
+  confirmer();
+}
+
+int confirmer() {
+  printf("\nWould you like to continue?\t");
+  scanf("%d", &confirm);
+  if (!confirm) {
+    printf("Goodbye!");
+    exit(0);
+  }
+  printf("\nOk next question.");
+  return 0;
 }
